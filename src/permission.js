3da8ba4ba1bfffe22fed9c0cc7da0fe477e2087c
Major333 <962461330@qq.com>
2020-08-04 16:34:28 +0800

    fix permission bug

--- src/permission.js
+++ src/permission.js
@@ -28,38 +28,6 @@ var menuQueryMap = {
 function checkToken(to, from, next) {
   if (storage.get(ACCESS_TOKEN)) {
     console.log('have token')
-    if (to.path === loginRoutePath) {
-      console.log('will lead you to your home page')
-      next({ path: defaultRoutePath })
-      NProgress.done()
-    } else {
-      if (store.getters.addRouters.length === 0) {
-        // TODO : role filter
-        const role = store.getters.role
-        // alert('hello')
-        store.dispatch('GenerateRoutes', { role }).then(() => {
-          router.addRoutes(store.getters.addRouters)
-          const redirect = decodeURIComponent(from.query.redirect || to.path)
-          // next({ path: redirect })
-          if (to.path === redirect) {
-            // set the replace: true so the navigation will not leave a history record
-            next({ ...to, replace: true })
-          } else {
-            // 跳转到目的路由
-            next({ path: redirect })
-          }
-        })
-      } else {
-        let menuItem = to.path.split('/').pop()
-        console.log('看这里！！！！！', to)
-        if (menuItem in menuQueryMap && Object.keys(to.params).length === 0) {
-          let params = menuQueryMap[menuItem]()
-          next({ name: to.name, params: params, replace: true })
-        } else {
-          next()
-        }
-      }
-    }
   } else {
     if (whiteList.includes(to.name)) {
       // 在免登录白名单，直接进入
@@ -73,13 +41,59 @@ function checkToken(to, from, next) {
 
 router.beforeEach((to, from, next) => {
   NProgress.start() // start progress bar
-  if (!store.getters.username) {
-    store.dispatch('Login').then((response) => {
-      console.log('in begin login response is :', response)
-      checkToken(to, from, next)
-    })
+  if (storage.get(ACCESS_TOKEN)) {
+    if (!store.getters.username) {
+      store
+        .dispatch('Login', { token: storage.get(ACCESS_TOKEN) })
+        .then((response) => {
+          console.log('in begin login response is :', response)
+          if (to.path === loginRoutePath) {
+            console.log('will lead you to your home page')
+            next({ path: defaultRoutePath })
+            NProgress.done()
+          } else {
+            if (store.getters.addRouters.length === 0) {
+              // TODO : role filter
+              const role = store.getters.role
+              // alert('hello')
+              store.dispatch('GenerateRoutes', { role }).then(() => {
+                router.addRoutes(store.getters.addRouters)
+                const redirect = decodeURIComponent(from.query.redirect || to.path)
+                // next({ path: redirect })
+                if (to.path === redirect) {
+                  // set the replace: true so the navigation will not leave a history record
+                  next({ ...to, replace: true })
+                } else {
+                  // 跳转到目的路由
+                  next({ path: redirect })
+                }
+              })
+            } else {
+              let menuItem = to.path.split('/').pop()
+              console.log('打印：menuItem: ')
+              console.log('看这里！！！！！', to)
+              if (menuItem in menuQueryMap && Object.keys(to.query).length === 0) {
+                let query = menuQueryMap[menuItem]()
+                next({ ...to, query: query, replace: true })
+              } else {
+                next()
+              }
+            }
+          }
+        })
+        .catch((error) => {
+          console.log('in begin login error is:', error)
+          store.dispatch('Logout')
+        })
+    }
   } else {
-    checkToken(to, from, next)
+    if (whiteList.includes(to.name)) {
+      // 在免登录白名单，直接进入
+      next()
+    } else {
+      next({ path: loginRoutePath })
+      NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
+    }
   }
 })
 
