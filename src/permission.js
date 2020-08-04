c18175666c1a3be7d03211881390ac5ed3827886
Major333 <962461330@qq.com>
2020-08-04 16:45:55 +0800

    add a new branch in permission

--- src/permission.js
+++ src/permission.js
@@ -25,16 +25,38 @@ var menuQueryMap = {
   }),
 }
 
-function checkToken(to, from, next) {
-  if (storage.get(ACCESS_TOKEN)) {
-    console.log('have token')
+function handle(to, from, next) {
+  if (to.path === loginRoutePath) {
+    console.log('will lead you to your home page')
+    next({ path: defaultRoutePath })
+    NProgress.done()
   } else {
-    if (whiteList.includes(to.name)) {
-      // 在免登录白名单，直接进入
-      next()
+    if (store.getters.addRouters.length === 0) {
+      // TODO : role filter
+      const role = store.getters.role
+      // alert('hello')
+      store.dispatch('GenerateRoutes', { role }).then(() => {
+        router.addRoutes(store.getters.addRouters)
+        const redirect = decodeURIComponent(from.query.redirect || to.path)
+        // next({ path: redirect })
+        if (to.path === redirect) {
+          // set the replace: true so the navigation will not leave a history record
+          next({ ...to, replace: true })
+        } else {
+          // 跳转到目的路由
+          next({ path: redirect })
+        }
+      })
     } else {
-      next({ path: loginRoutePath })
-      NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
+      let menuItem = to.path.split('/').pop()
+      console.log('打印：menuItem: ')
+      console.log('看这里！！！！！', to)
+      if (menuItem in menuQueryMap && Object.keys(to.query).length === 0) {
+        let query = menuQueryMap[menuItem]()
+        next({ ...to, query: query, replace: true })
+      } else {
+        next()
+      }
     }
   }
 }
@@ -46,45 +68,14 @@ router.beforeEach((to, from, next) => {
       store
         .dispatch('Login', { token: storage.get(ACCESS_TOKEN) })
         .then((response) => {
-          console.log('in begin login response is :', response)
-          if (to.path === loginRoutePath) {
-            console.log('will lead you to your home page')
-            next({ path: defaultRoutePath })
-            NProgress.done()
-          } else {
-            if (store.getters.addRouters.length === 0) {
-              // TODO : role filter
-              const role = store.getters.role
-              // alert('hello')
-              store.dispatch('GenerateRoutes', { role }).then(() => {
-                router.addRoutes(store.getters.addRouters)
-                const redirect = decodeURIComponent(from.query.redirect || to.path)
-                // next({ path: redirect })
-                if (to.path === redirect) {
-                  // set the replace: true so the navigation will not leave a history record
-                  next({ ...to, replace: true })
-                } else {
-                  // 跳转到目的路由
-                  next({ path: redirect })
-                }
-              })
-            } else {
-              let menuItem = to.path.split('/').pop()
-              console.log('打印：menuItem: ')
-              console.log('看这里！！！！！', to)
-              if (menuItem in menuQueryMap && Object.keys(to.query).length === 0) {
-                let query = menuQueryMap[menuItem]()
-                next({ ...to, query: query, replace: true })
-              } else {
-                next()
-              }
-            }
-          }
+          handle(to, from, next)
         })
         .catch((error) => {
           console.log('in begin login error is:', error)
           store.dispatch('Logout')
         })
+    } else {
+      handle(to, from, next)
     }
   } else {
     if (whiteList.includes(to.name)) {
