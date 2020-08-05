80f9f5f12a2189d4160253eeb4a1cd34d6b803d6
Xuedixuedi <958190137@qq.com>
2020-08-05 17:27:59 +0800

    新增 v-charts 依赖

--- src/views/statistics/Statistics.vue
+++ src/views/statistics/Statistics.vue
@@ -7,13 +7,34 @@
       </a-breadcrumb>
       <a-row :gutter="24" class="row">
         <a-col :sm="24" :md="10" class="col">
-          <chart-card title="总销售量">
-            <a-icon type="buy" slot="action" />
-            <span slot="total">$ 100.00</span>
-            <span>
-              周同比 12%
-            </span>
-          </chart-card>
+          <a-card
+            class="antd-pro-pages-dashboard-analysis-salesCard"
+            :loading="loading"
+            :bordered="false"
+            title="任务按执行者分布"
+            :style="{ height: '100%' }"
+          >
+            <div slot="extra" style="height: inherit;">
+              <div class="analysis-salesTypeRadio">
+                <a-radio-group default-value="a">
+                  <a-radio-button value="a">全部任务</a-radio-button>
+                  <a-radio-button value="b">未完成</a-radio-button>
+                  <a-radio-button value="c">已完成</a-radio-button>
+                </a-radio-group>
+              </div>
+            </div>
+            <div>
+              <div>
+                <v-chart :force-fit="true" :height="405" :data="pieData" :scale="pieScale">
+                  <v-tooltip :show-title="false" data-key="item*percent" />
+                  <v-axis />
+                  <v-legend data-key="item" />
+                  <v-pie position="percent" color="item" :v-style="pieStyle" />
+                  <v-coord type="theta" :radius="0.75" :inner-radius="0.6" />
+                </v-chart>
+              </div>
+            </div>
+          </a-card>
         </a-col>
         <a-col :sm="24" :md="10" class="col">
           <a-statistic title="Active Users" :value="112893" style="margin-right: 50px;" />
@@ -29,12 +50,73 @@
 </template>
 
 <script>
+import {
+  ChartCard,
+  MiniArea,
+  MiniBar,
+  MiniProgress,
+  RankList,
+  Bar,
+  Trend,
+  NumberInfo,
+  MiniSmoothArea,
+} from '@/components'
+
+const DataSet = require('@antv/data-set')
+
+const sourceData = [
+  { item: '无负责人', count: 32.2 },
+  { item: '蔡徐坤', count: 21 },
+  { item: 'Tom', count: 17 },
+  { item: 'lxd', count: 5 },
+]
+
+const pieScale = [
+  {
+    dataKey: 'percent',
+    min: 0,
+    formatter: '.0%',
+  },
+]
+
+const dv = new DataSet.View().source(sourceData)
+dv.transform({
+  type: 'percent',
+  field: 'count',
+  dimension: 'item',
+  as: 'percent',
+})
+const pieData = dv.rows
+
 export default {
   name: 'Statistics',
+  components: {
+    ChartCard,
+    MiniArea,
+    MiniBar,
+    MiniProgress,
+    RankList,
+    Bar,
+    Trend,
+    NumberInfo,
+    MiniSmoothArea,
+  },
+  data() {
+    return {
+      //
+      pieScale,
+      pieData,
+      sourceData,
+      pieStyle: {
+        stroke: '#fff',
+        lineWidth: 1,
+      },
+    }
+  },
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .row {
   padding: '24px';
   min-height: '360px';
@@ -45,5 +127,35 @@ export default {
   background: #ffffff;
   margin-bottom: 24px;
   margin-left: 24px;
+}
+
+.antd-pro-pages-dashboard-analysis-twoColLayout {
+  position: relative;
+  display: flex;
+  display: block;
+  flex-flow: row wrap;
+}
+
+.antd-pro-pages-dashboard-analysis-salesCard {
+  height: calc(100% - 24px);
+  /deep/ .ant-card-head {
+    position: relative;
+  }
+}
+
+.dashboard-analysis-iconGroup {
+  i {
+    margin-left: 16px;
+    color: rgba(0, 0, 0, 0.45);
+    cursor: pointer;
+    transition: color 0.32s;
+    color: black;
+  }
+}
+
+.analysis-salesTypeRadio {
+  position: absolute;
+  right: 20px;
+  bottom: 12px;
 }
 </style>
