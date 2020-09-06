855a5804c3ca6819864499c77e4763d572518706
Rika <40230165+Moreonenight@users.noreply.github.com>
2020-09-06 19:41:47 +0800

    删除多余代码

--- src/views/management/index.vue
+++ src/views/management/index.vue
@@ -7,145 +7,22 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {} from 'vuex'
 
 export default {
   components: {},
   data() {
-    return {
-      tags: ['很有想法的', '专注设计', '辣~', '大长腿', '川妹子', '海纳百川'],
-
-      tagInputVisible: false,
-      tagInputValue: '',
-
-      teams: [],
-      teamSpinning: true,
-
-      tabListNoTitle: [
-        {
-          key: 'article',
-          tab: '公告(8)',
-        },
-        {
-          key: 'app',
-          tab: '任务(8)',
-        },
-        {
-          key: 'project',
-          tab: '通知(8)',
-        },
-      ],
-      noTitleKey: 'app',
-    }
+    return {}
   },
-  computed: {
-    ...mapGetters(['nickname', 'avatar']),
-  },
-  mounted() {
-    this.getTeams()
-  },
-  methods: {
-    getTeams() {
-      this.$http.get('/workplace/teams').then((res) => {
-        this.teams = res.result
-        this.teamSpinning = false
-      })
-    },
-
-    handleTabChange(key, type) {
-      this[type] = key
-    },
-
-    handleTagClose(removeTag) {
-      const tags = this.tags.filter((tag) => tag !== removeTag)
-      this.tags = tags
-    },
-
-    showTagInput() {
-      this.tagInputVisible = true
-      this.$nextTick(() => {
-        this.$refs.tagInput.focus()
-      })
-    },
-
-    handleInputChange(e) {
-      this.tagInputValue = e.target.value
-    },
-
-    handleTagInputConfirm() {
-      const inputValue = this.tagInputValue
-      let { tags } = this
-      if (inputValue && !tags.includes(inputValue)) {
-        tags = [...tags, inputValue]
-      }
-
-      Object.assign(this, {
-        tags,
-        tagInputVisible: false,
-        tagInputValue: '',
-      })
-    },
-  },
+  computed: {},
+  mounted() {},
+  methods: {},
 }
 </script>
 
-<style lang="less" scoped>
+<style scoped>
 .ant-pro-page-header-search {
   text-align: center;
   margin-bottom: 16px;
-}
-.avatar-upload-wrapper {
-  height: 200px;
-  width: 100%;
-}
-
-.ant-upload-preview {
-  position: relative;
-  margin: 0 auto;
-  width: 100%;
-  max-width: 180px;
-  border-radius: 50%;
-  box-shadow: 0 0 4px #ccc;
-
-  .upload-icon {
-    position: absolute;
-    top: 0;
-    right: 10px;
-    font-size: 1.4rem;
-    padding: 0.5rem;
-    background: rgba(222, 221, 221, 0.7);
-    border-radius: 50%;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-  }
-  .mask {
-    opacity: 0;
-    position: absolute;
-    background: rgba(0, 0, 0, 0.4);
-    cursor: pointer;
-    transition: opacity 0.4s;
-
-    &:hover {
-      opacity: 1;
-    }
-
-    i {
-      font-size: 2rem;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin-left: -1rem;
-      margin-top: -1rem;
-      color: #d6d6d6;
-    }
-  }
-
-  img,
-  .mask {
-    width: 100%;
-    max-width: 180px;
-    height: 100%;
-    border-radius: 50%;
-    overflow: hidden;
-  }
 }
 </style>
