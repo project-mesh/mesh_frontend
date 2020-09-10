5e1d3dceb761c5c7553221cf7ef5dc9c38080ebc
HR8398Cephei <z2687537622@gmail.com>
2020-09-11 01:48:25 +0800

    合并管理员数据页 添加部分功能

--- src/views/management/AdminStatistics.vue
+++ src/views/management/AdminStatistics.vue
@@ -31,33 +31,20 @@
 </template>
 
 <script>
-import projectMixin from '@/utils/mixins/projectMixin'
-import teamMixin from '@/utils/mixins/teamMixin'
 import China from 'echarts/map/json/china.json'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Statistics',
   components: {},
-  mixins: [teamMixin, projectMixin],
   data() {
     this.mapChartSettings = {
       mapOrigin: China,
     }
     this.histoChartSettings = {
-      axisSite: { right: ['下单率'] },
-      yAxisType: ['KMB', 'percent'],
-      yAxisName: ['数值', '比率'],
+      yAxisName: ['用户数'],
     }
     return {
-      mapChartData: {
-        columns: ['位置', '税收', '人口', '面积'],
-        rows: [
-          { 位置: '吉林', 税收: 123, 人口: 123, 面积: 92134 },
-          { 位置: '北京', 税收: 1223, 人口: 2123, 面积: 29234 },
-          { 位置: '上海', 税收: 2123, 人口: 1243, 面积: 94234 },
-          { 位置: '浙江', 税收: 4123, 人口: 5123, 面积: 29234 },
-        ],
-      },
       lineChartData: {
         columns: ['日期', '访问用户', '下单用户', '下单率'],
         rows: [
@@ -69,32 +56,126 @@ export default {
           { 日期: '1/6', 访问用户: 4593, 下单用户: 4293, 下单率: 0.78 },
         ],
       },
-      histoChartData: {
-        columns: ['日期', '访问用户', '下单用户', '下单率'],
-        rows: [
-          { 日期: '1/1', 访问用户: 1393, 下单用户: 1093, 下单率: 0.32 },
-          { 日期: '1/2', 访问用户: 3530, 下单用户: 3230, 下单率: 0.26 },
-          { 日期: '1/3', 访问用户: 2923, 下单用户: 2623, 下单率: 0.76 },
-          { 日期: '1/4', 访问用户: 1723, 下单用户: 1423, 下单率: 0.49 },
-          { 日期: '1/5', 访问用户: 3792, 下单用户: 3492, 下单率: 0.323 },
-          { 日期: '1/6', 访问用户: 4593, 下单用户: 4293, 下单率: 0.78 },
-        ],
-      },
-      pieChartData: {
-        columns: ['日期', '访问用户'],
-        rows: [
-          { 日期: '1/1', 访问用户: 1393 },
-          { 日期: '1/2', 访问用户: 3530 },
-          { 日期: '1/3', 访问用户: 2923 },
-          { 日期: '1/4', 访问用户: 1723 },
-          { 日期: '1/5', 访问用户: 3792 },
-          { 日期: '1/6', 访问用户: 4593 },
-        ],
-      },
+      // histoChartData: {
+      //   columns: ['日期', '访问用户', '下单用户', '下单率'],
+      //   rows: [
+      //     { 日期: '1/1', 访问用户: 1393, 下单用户: 1093, 下单率: 0.32 },
+      //     { 日期: '1/2', 访问用户: 3530, 下单用户: 3230, 下单率: 0.26 },
+      //     { 日期: '1/3', 访问用户: 2923, 下单用户: 2623, 下单率: 0.76 },
+      //     { 日期: '1/4', 访问用户: 1723, 下单用户: 1423, 下单率: 0.49 },
+      //     { 日期: '1/5', 访问用户: 3792, 下单用户: 3492, 下单率: 0.323 },
+      //     { 日期: '1/6', 访问用户: 4593, 下单用户: 4293, 下单率: 0.78 },
+      //   ],
+      // },
     }
   },
-  computed: {},
+  computed: {
+    ...mapGetters([
+      'maleUser',
+      'femaleUser',
+      'unknownUser',
+      'userAge',
+      'userLocation',
+      'currentOnlineUser',
+      'avgTeamUser',
+      'avgTeamProject',
+      'currentTotalUser',
+      'historyTotalUser',
+    ]),
+    histoChartData() {
+      const columns = ['日期', '累积用户数']
+      const rows = []
+
+      let year = new Date().getFullYear()
+      let month = new Date().getMonth() + 1
+
+      for (let i = 0; i < this.historyTotalUser.length; ++i) {
+        rows.push({
+          日期: `${year}年${month}月`,
+          累积用户数: this.historyTotalUser[i].totalUser,
+        })
+
+        if (--month === 0) {
+          --year
+          month = 12
+        }
+      }
+
+      return { columns, rows: rows.reverse() }
+    },
+    //     mapChartData: {
+    //   columns: ['位置', '税收', '人口', '面积'],
+    //   rows: [
+    //     { 位置: '吉林', 税收: 123, 人口: 123, 面积: 92134 },
+    //     { 位置: '北京', 税收: 1223, 人口: 2123, 面积: 29234 },
+    //     { 位置: '上海', 税收: 2123, 人口: 1243, 面积: 94234 },
+    //     { 位置: '浙江', 税收: 4123, 人口: 5123, 面积: 29234 },
+    //   ],
+    // },
+    mapChartData() {
+      console.log('userlocation: ', this.userLocation)
+      const columns = ['位置', '用户数']
+      const rows = []
+
+      for (let i = 0; i < this.userLocation.length; ++i) {
+        let location = this.userLocation[i].location
+
+        if (location.endsWith('省')) {
+          location = location.slice(0, -1)
+        } else if (location.endsWith('族自治区') || location.endsWith('特别行政区')) {
+          location = location.slice(0, -5)
+        } else if (location.endsWith('自治区')) {
+          location = location.slice(0, -3)
+        }
+        rows.push({
+          位置: location,
+          用户数: this.userLocation[i].userCount,
+        })
+      }
+
+      return {
+        columns,
+        rows,
+      }
+    },
+    //     pieChartData: {
+    //   columns: ['日期', '访问用户'],
+    //   rows: [
+    //     { 日期: '1/1', 访问用户: 1393 },
+    //     { 日期: '1/2', 访问用户: 3530 },
+    //     { 日期: '1/3', 访问用户: 2923 },
+    //     { 日期: '1/4', 访问用户: 1723 },
+    //     { 日期: '1/5', 访问用户: 3792 },
+    //     { 日期: '1/6', 访问用户: 4593 },
+    //   ],
+    // },
+    pieChartData() {
+      const columns = ['性别', '用户数']
+      const rows = []
+
+      rows.push(
+        {
+          性别: '男',
+          用户数: this.maleUser,
+        },
+        {
+          性别: '女',
+          用户数: this.femaleUser,
+        },
+        {
+          性别: '未知',
+          用户数: this.unknownUser,
+        }
+      )
+
+      return {
+        columns,
+        rows,
+      }
+    },
+  },
   methods: {
+    ...mapActions(['queryGeneralStatistics', 'queryTotalUser', 'queryUserStatistics']),
     //导出的方法
     export2Excel() {
       require.ensure([], () => {
@@ -120,7 +201,16 @@ export default {
     },
   },
   mounted() {
-    this.$emit('load', 'statistics')
+    Promise.all([
+      this.queryGeneralStatistics(),
+      this.queryUserStatistics(),
+      this.queryTotalUser({
+        timeInterval: 10,
+        itemCount: 10,
+      }).catch((err) => {
+        console.error('error in query statistics, error: ', err)
+      }),
+    ])
   },
 }
 </script>
