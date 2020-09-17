e8a56b2809a068fc3e0ef360c8d32674e772ff77
Rika <40230165+Moreonenight@users.noreply.github.com>
2020-09-17 19:10:24 +0800

    Update Latex.vue

--- src/views/toolBox/Latex.vue
+++ src/views/toolBox/Latex.vue
@@ -1,15 +1,33 @@
 <template>
-  <vue-friendly-iframe
-    class-name="vue-friendly-iframe"
-    src="https://sagecell.sagemath.org/"
-    scrolling="no"
-  >
-    您的浏览器不支持 iframe 标签，请考虑更换 Edge/Firefox/Chrome 等现代浏览器
-  </vue-friendly-iframe>
+  <div>
+    <a-button type="primary" @click="test">Primary</a-button>
+    <vue-friendly-iframe
+      class-name="vue-friendly-iframe"
+      src="https://sagecell.sagemath.org/"
+      scrolling="no"
+      id="iframe-sagecell"
+    >
+      您的浏览器不支持 iframe 标签，请考虑更换 Edge/Firefox/Chrome 等现代浏览器
+    </vue-friendly-iframe>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    mounted() {
+      var iframe = document.getElementById('iframe-sagecell').firstElementChild
+      var elmnt = iframe.contentWindow.document.getElementById('sagecell_about')
+      elmnt.style.display = 'none'
+    },
+    test() {
+      var iframe = document.getElementById('iframe-sagecell').firstElementChild
+      console.log(iframe)
+      var elmnt = iframe.contentWindow.document.getElementById('sagecell_about')
+      elmnt.style.display = 'none'
+    },
+  },
+}
 </script>
 
 <style>
