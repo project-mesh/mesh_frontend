1d04b3381916b14a57287cb2c9d82b28b50cd4f2
Mrcopytuo <920411694@qq.com>
2020-09-17 16:38:03 +0800

    希望能过

--- src/views/toolBox/Latex.vue
+++ src/views/toolBox/Latex.vue
@@ -1,7 +1,7 @@
 <template>
-  <vue-friendly-iframe class="vue-friendly-iframe" src="http://www.baidu.com">
+  <iframe class="vue-friendly-iframe" src="http://www.baidu.com">
     IE：你们都看我干吗，我现在也是支持的
-  </vue-friendly-iframe>
+  </iframe>
 </template>
 
 <script>
