52c1584926955add02ddafe4cf81a2b1be4652af
Xuedixuedi <958190137@qq.com>
2020-09-18 15:33:24 +0800

    delete avatar

--- src/views/project/projectDetails/taskBoard/TaskInfo.vue
+++ src/views/project/projectDetails/taskBoard/TaskInfo.vue
@@ -15,9 +15,6 @@
         {{ task.createTime | dateFilter }}
       </a-descriptions-item>
       <a-descriptions-item label="截止日期" :span="4">{{ task.deadline }}</a-descriptions-item>
-      <a-descriptions-item label="创建者" :span="4">
-        <avatar-featured-user :username="task.founder" />
-      </a-descriptions-item>
       <a-descriptions-item label="负责人" :span="4">
         <avatar-featured-user :username="task.principal" />
       </a-descriptions-item>
