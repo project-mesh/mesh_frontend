7b9aac2e0df1a3ad2346a369fc98f5417d2ea993
rand1925 <rand1925@outlook.com>
2021-06-15 21:45:46 +0800

    修复甘特图

--- src/views/project/projectDetails/gantt/BasicGantt.vue
+++ src/views/project/projectDetails/gantt/BasicGantt.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="toolbox">
-      <!-- <button @click="updateFirstRow">Update first row</button>
-     -->
-      <a-button-group size="large">
+      <a-checkbox @change="onOnlyNotFinishedChange">仅显示未完成</a-checkbox>
+      <a-checkbox @change="onOnlyViewMineChange">只看我负责的</a-checkbox>
+      <a-button-group>
         <a-button type="primary" @click="increaseZoomLevel">
           <a-icon type="minus" />
           缩小
@@ -38,10 +38,20 @@ export default {
   computed: {
     ...mapGetters(['projectId', 'projectName', 'username', 'projectTasks', 'projectSubTasks']),
   },
+  data() {
+    return {
+      onlyNotFinished: false,
+      onlyViewMine: false,
+      rows: null,
+      items: null,
+    }
+  },
   mounted() {
     /**
      * @type { import("gantt-schedule-timeline-calendar").Config }
      */
+    this.rows = this.generateRows()
+    this.items = this.generateItems()
     const config = {
       licenseKey:
         '====BEGIN LICENSE KEY====\nEZ4nLvdzgCbYHV7YqRSsysJtJvU6l5/g62Z14TOzjeDL2SfpqCPQpNR7AWDLaStjCs83TITTxEVkHuIPX91YlLpIcMAv24QWf4w4RZFbY9eHjGNwNDnqv+Vj6zRNGruBQX5SFXy7q5JldR4qdsoYk0asN6Wyw2FlRXVF0zndqi9q+QfJp8lNQHaApeCFlWcgmWkCFIURlhNWYruX426wkO1hCwKp8m3bPiVVh1ZBqXdNXhcUGW7u5rjDBJfo2FyTHNbJNlvjIP5u9A0qF+cIPbyQpjaa1lp7uGHq9RaA6pPpXYN4cFZjExeeffp6tCmzM/pPih9sFLMmDTcI+YTeNg==||U2FsdGVkX1/FfAGX5lFg5DQDv9D3CYXSGv+kSNWyToyEOyZSQf/BIq4SP9Ydc00gR7D6pwYsIKPydeV3a6dwH07pFfnmOMg7yntc3W6oIDY=\nBlO99icAouFBbzGIi/Psq1MzhKdP94Ug7f9ApsUb3nLKZoCcxwwW2hxxy73mbhJoZJDCDyk+jvny7gKyfOq3Tn0QXL0lUofmvX7Akobp1ymwfakgV2YuEo8O5UPoZp6fju8jCexBChhwMbr30k0BhmJVjXEYl0uxaycB36E37fj25vYVgERxcK/R7O0z45EVmNTLzMpldObt58WmFikmczam+nGGDShOuAYonrMjMNCUc1J7VO6VL/419qS+Qpe80ChXaUtDS81f/Pyc1QZEstzHLvncsgzniJxvxVurujef0o8HZ+z9lyqScU+1m4Bb9WhAzEIX+FoIMHBxsrr2tg==\n====END LICENSE KEY====',
@@ -68,10 +78,10 @@ export default {
             },
           },
         },
-        rows: this.generateRows(),
+        rows: this.rows,
       },
       chart: {
-        items: this.generateItems(),
+        items: this.items,
       },
     }
 
@@ -88,10 +98,31 @@ export default {
   },
   methods: {
     ...mapActions(['queryTeamTasks', 'updateTask']),
-    updateFirstRow() {
-      state.update(`config.list.rows.${GSTC.api.GSTCID('0')}`, (row) => {
-        row.label = 'Changed dynamically'
-        return row
+    filterRows() {
+      let res = {}
+      for (const key in this.rows) {
+        const value = this.rows[key]
+        if (this.onlyViewMine && value.principal != this.username) {
+          continue
+        }
+        if (this.onlyNotFinished && value.isFinished) {
+          continue
+        }
+        res[key] = value
+      }
+      return res
+    },
+    onOnlyNotFinishedChange(e) {
+      this.onlyNotFinished = e.target.checked
+      state.update('config.list.rows', (rows) => {
+        return this.filterRows()
+      })
+    },
+
+    onOnlyViewMineChange(e) {
+      this.onlyViewMine = e.target.checked
+      state.update('config.list.rows', (rows) => {
+        return this.filterRows()
       })
     },
 
@@ -106,23 +137,22 @@ export default {
         return o - 1
       })
     },
+
     /**
      * @returns { import("gantt-schedule-timeline-calendar").Items }
      */
     generateItems() {
+      const priorityColor = ['#1890FF', '#52C41A', '#FAAD14', '#F5222D']
       const items = {}
-      for (let i = 0; i < this.projectTasks.length; i++) {
-        const task = this.projectTasks[i]
+      for (const task of this.projectTasks) {
         const taskId = task.taskId
-        const founder = task.founder
         const id = GSTC.api.GSTCID('t' + taskId.toString())
         const rowId = GSTC.api.GSTCID(taskId.toString())
         const startTime = GSTC.api.date(task.createTime).startOf('day').valueOf()
         const endTime = GSTC.api.date(task.deadline).endOf('day').valueOf()
-        const priorityColor = ['#1890FF', '#52C41A', '#FAAD14', '#F5222D']
         items[id] = {
           id,
-          label: founder,
+          label: task.principal,
           rowId,
           style: {
             background: priorityColor[task.priority],
@@ -140,12 +170,13 @@ export default {
      */
     generateRows() {
       const rows = {}
-      for (let i = 0; i < this.projectTasks.length; i++) {
-        const task = this.projectTasks[i]
+      for (const task of this.projectTasks) {
         const taskId = task.taskId
         const id = GSTC.api.GSTCID(taskId.toString())
         rows[id] = {
           id,
+          isFinished: task.isFinished,
+          principal: task.principal,
           label: task.taskName,
         }
       }
